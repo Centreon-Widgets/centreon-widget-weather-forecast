cea43fddaba20aefb10043489f2dfd542481aa6f
sc979 <34628915+sc979@users.noreply.github.com>
2021-02-12 11:00:18 +0100

    fix(secu): use jQuery 3.5.1 min file (#5)

--- weather-forecast/index.php
+++ weather-forecast/index.php
@@ -78,7 +78,7 @@ try {
         <link href="<?php echo '../../Themes/Centreon-2/Color/blue_css.php';?>" rel="stylesheet" type="text/css"/>
         <link href="src/test.css" rel="stylesheet" type="text/css"/>
         <script src="../../include/common/javascript/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="../../include/common/javascript/jquery/jquery.js"></script>
+        <script type="text/javascript" src="../../include/common/javascript/jquery/jquery.min.js"></script>
     	<script type="text/javascript" src="../../include/common/javascript/jquery/jquery-ui.js"></script>
         <script type="text/javascript" src="../../include/common/javascript/widgetUtils.js"></script>
     <style type="text/css">
